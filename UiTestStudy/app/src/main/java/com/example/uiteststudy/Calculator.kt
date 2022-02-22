eeab7b642de368cfffe5753de2f0bbd8e443645d
HeeGyeong <fhskf94kr@naver.com>
2022-02-22 21:17:34 +0900

    trust 적용 도중 커밋

---
+++ UiTestStudy/app/src/main/java/com/example/uiteststudy/Calculator.kt
@@ -0,0 +1,12 @@
+package com.example.uiteststudy
+
+class Calculator {
+
+    fun plusNum(num: Int, num2: Int): Int {
+        return num + num2
+    }
+
+    fun multiNum(num: Int, num2: Int): Int {
+        return num * num2
+    }
+}
--- UiTestStudy/app/src/test/java/com/example/uiteststudy/ExampleUnitTest.kt
+++ UiTestStudy/app/src/test/java/com/example/uiteststudy/ExampleUnitTest.kt
@@ -1,17 +1,36 @@
 package com.example.uiteststudy
 
 import org.junit.Test
-
-import org.junit.Assert.*
+import org.junit.Before
+import com.google.common.truth.Truth.assertThat
 
 /**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
+ * Truth 를 사용한 예제 작성 중 입니다.
  */
 class ExampleUnitTest {
+    private lateinit var calCul: Calculator
+
+    @Before
+    fun before() {
+        calCul = Calculator()
+    }
+
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun plus_test() {
+        val result = calCul.plusNum(10,20)
+        assertThat(result).isEqualTo(40)
+    }
+
+    @Test
+    fun multi_test() {
+        val result = calCul.multiNum(10, 10)
+        assertThat(result).isEqualTo(100)
+        assertThat(result + 10).isEqualTo(110)
+        assertThat(result).isNotNull()
+        plus_test()
+    }
+
+    fun sample(): Int {
+        return 10
     }
 }
