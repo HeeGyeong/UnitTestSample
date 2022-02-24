b726612334ebdf7e076ee778e70c2ecb409e208b
HeeGyeong <fhskf94kr@naver.com>
2022-02-24 21:53:33 +0900

    mockito 적용

---
+++ UiTestStudy/app/src/main/java/com/example/uiteststudy/MockSample.kt
@@ -0,0 +1,16 @@
+package com.example.uiteststudy
+
+class MockSample {
+
+    fun getNumber(): Int {
+        return 1
+    }
+
+    fun getString(): String {
+        return "get String"
+    }
+
+    fun getList(): List<String> {
+        return listOf("123")
+    }
+}
---
+++ UiTestStudy/app/src/test/java/com/example/uiteststudy/MockSampleTest.kt
@@ -0,0 +1,67 @@
+package com.example.uiteststudy
+
+import com.google.common.truth.Truth.assertThat
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Mockito
+import org.mockito.Mockito.never
+import org.mockito.Mockito.timeout
+import org.mockito.Mockito.times
+
+class MockSampleTest {
+
+    private lateinit var mock: MockSample
+
+    @Before
+    fun before() {
+        mock = Mockito.mock(MockSample::class.java)
+        // when 함수를 사용하여 Mock 객체의 행동 설정. Test Stub.
+        Mockito.`when`(mock.getNumber()).thenReturn(222)
+        Mockito.`when`(mock.getString()).thenReturn("100")
+        //Mockito.`when`(mock.getList()).
+    }
+
+    @Test
+    fun mockito_test() {
+        // Mock 객체에 넣은 값을 확인.
+        assertThat(mock.getNumber()).isEqualTo(222)
+        assertThat(mock.getString()).contains("100")
+    }
+
+    @Test
+    fun mockito_answer_test() {
+        answer()
+
+        assertThat(mock.getNumber()).isEqualTo(369)
+        assertThat(mock.getList()).hasSize(1)
+    }
+
+    @Test
+    fun verify_test() {
+        mock.getString()
+        mock.getList()
+
+        Mockito.verify(mock).getList() // 호출이 되었는가
+        Mockito.verify(mock, times(1)).getString() // 1회 호출 되었는가
+        Mockito.verify(mock, never()).getNumber() // 호출을 하지 않았는가
+
+        mock.getString()
+        Mockito.verify(mock, times(2)).getString() // 2회 호출 되었는가
+        //Mockito.verify(mock, Mockito.)
+    }
+
+    private fun answer() {
+        Mockito.`when`(mock.getList()).thenAnswer {
+            println("print Message")
+
+            return@thenAnswer listOf("mock")
+        }
+
+        Mockito.`when`(mock.getNumber()).thenAnswer {
+            println("mock.getNumber ? ${it.arguments.size}")
+
+            return@thenAnswer 369
+        }
+
+    }
+}
